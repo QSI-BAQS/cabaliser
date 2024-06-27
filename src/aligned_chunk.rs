6df1a6a2eba7d3e9cd63ebdf533faf4991d6dc6b
Alan Robertson <alan@qrop.xyz>
2024-06-27 14:10:13 +1000

    Experiments with generic iterators

--- src/aligned_chunk.rs
+++ src/aligned_chunk.rs
@@ -1,26 +1,34 @@
 ////mod consts;
 //use crate::CACHE_LINE_SIZE;
 //use crate::USIZE_CACHE_LINE;
-use std::ops;
 use std::mem;
+use std::ops;
 
 macro_rules! BITS_TO_BYTE {
-   () => {8}
+    () => {
+        8
+    };
 }
 
 #[macro_export]
 macro_rules! CACHE_LINE_SIZE {
-   () => {64}
+    () => {
+        64
+    };
 }
 
 #[macro_export]
-    macro_rules! CACHE_LINE_SIZE_BITS {
-       () => {CACHE_LINE_SIZE!() * BITS_TO_BYTE!()}
+macro_rules! CACHE_LINE_SIZE_BITS {
+    () => {
+        CACHE_LINE_SIZE!() * BITS_TO_BYTE!()
+    };
 }
 
 #[macro_export]
 macro_rules! USIZE_CACHE_LINE {
-    () => {CACHE_LINE_SIZE!() / mem::size_of::<usize>()}
+    () => {
+        CACHE_LINE_SIZE!() / mem::size_of::<usize>()
+    };
 }
 
 /*
@@ -28,14 +36,63 @@ macro_rules! USIZE_CACHE_LINE {
  * Cache line aligned chunk object
  */
 #[repr(C, align(64))]
+//#[derive(Debug, Copy, Clone)]
 pub struct AlignedChunk {
     chunk: [usize; USIZE_CACHE_LINE!()],
 }
+
+// Provide your own size guarantees
+pub struct IterCounterSized<'a, T: ?Sized> {
+    reference : &'a mut T, 
+    counter: usize,
+    size : usize,
+}
+
+// Implements a Counter based iterator for array types [T]
+impl<'a, T: Copy + ops::Index<usize, Output = T>> Iterator for IterCounterSized<'a, [T]>{
+   type Item = T;  
+   fn next(&mut self) -> Option<Self::Item>
+   {
+       // End of iterator 
+        if (self.counter >= self.size) { 
+            return None;
+        }
+
+        // Return next item from iterator
+        self.counter += 1; 
+        return Some(self.reference[self.counter]); 
+   }
+}
+
+impl<'a, T: Copy + ops::Index<usize, Output = T>> Iterator for IterCounterSized<'a, Vec<T>>{
+   type Item = T;  
+   fn next(&mut self) -> Option<Self::Item>
+   {
+       // End of iterator 
+        if (self.counter >= self.size) { 
+            return None;
+        }
+
+        // Return next item from iterator
+        self.counter += 1; 
+        return Some(&self.reference[self.counter]); 
+   }
+}
+
 
 impl AlignedChunk {
     pub fn new() -> Self {
         return Self {
             chunk: [0; USIZE_CACHE_LINE!()],
+        };
+    }
+
+    pub fn iter(&mut self) -> IterCounterSized<[usize]> 
+    {
+        return IterCounterSized {
+            reference : &mut self.chunk,  
+            counter : 0,
+            size : USIZE_CACHE_LINE!(), 
         };
     }
 
@@ -51,12 +108,17 @@ impl AlignedChunk {
         }
     }
 
-    pub fn get(&mut self, idx : usize) -> &Self
-    {
+    pub fn get(&mut self, idx: usize) -> &mut usize {
         assert!(idx < USIZE_CACHE_LINE!());
-        return self.chunk[idx];
+        return &mut self.chunk[idx];
+    }
+
+    pub fn get_bit(&self, idx: usize) -> u8 {
+        return ((1 << (idx % USIZE_CACHE_LINE!())) & self.chunk[idx / USIZE_CACHE_LINE!()]) as u8;
     }
 }
+
+
 impl ops::BitXorAssign for AlignedChunk {
     fn bitxor_assign(&mut self, other: AlignedChunk) {
         for i in 0..self.chunk.len() {
@@ -64,6 +126,8 @@ impl ops::BitXorAssign for AlignedChunk {
         }
     }
 }
+// TODO impl ops::Index
+// And overwrite with the get method
 
 /*
  * AlignedChunks
@@ -110,9 +174,19 @@ impl AlignedChunks {
         }
     }
 
-    pub fn get_chunk(&mut self, idx : usize) -> &AlignedChunk 
-    {
+    pub fn get_chunk(&mut self, idx: usize) -> &AlignedChunk {
         assert!(idx < self.len());
         return &self.chunks[idx];
     }
+
+    pub fn iter(&mut self) -> IterCounterSized<Vec<AlignedChunk>> 
+    {
+        return IterCounterSized<Vec<AlignedChunk>> {
+            reference : &mut self.chunks,  
+            counter : 0,
+            size : self.len(), 
+        };
+    }
+
+
 }
