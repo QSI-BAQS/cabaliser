0954110bdf357f538713b9693cf90b13919bbd9d
Alan Robertson <alan@qrop.xyz>
2024-09-16 22:08:58 +1000

    Moved api functions to separate file:

---
+++ c/lib/widget_python_api.h
@@ -0,0 +1,43 @@
+#ifndef WIDGET_API_H
+#define WIDGET_API_H
+
+/*
+ * Functions for use with the python API
+ * Most of these accept a pointer that is handled by the python
+ * scope and then write values to the appropriate array.
+ *
+ */
+
+#include "widget.h"
+
+/*
+ * widget_get_adjacencies_api
+ * For a qubit in the tableau, list all adjacent qubits 
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: target_qubit : const size_t :: The target qubit 
+ * :: adj : struct adjacency_obj* :: Pointer to object to write to
+ * Returns a heap allocated array of uint32_t objects
+ */
+void widget_get_adjacencies_api(const widget_t* wid, const size_t target_qubit, struct adjacency_obj* adj);
+
+
+/*
+ * widget_get_io_map_api
+ * Returns an array of io map objects
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: arr : uint32_t** :: Pointer to io map to return 
+ * Writes the pointer to the io map to *arr
+ */
+void widget_get_io_map_api(const widget_t* wid, qubit_map_t** arr);
+
+/*
+ * widget_get_measurements_api
+ * Returns an array of measurements objects
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: arr : uint32_t** :: Pointer to io map to return 
+ * Writes the pointer to the io map to *arr
+ */
+void widget_get_measurements_api(const widget_t* wid, non_clifford_tag_t** arr);
+
+
+#endif
---
+++ c/src/widget_python_api.c
@@ -0,0 +1,61 @@
+#include "widget_python_api.h"
+
+/*
+ * widget_get_adjacencies_api
+ * For a qubit in the tableau, list all adjacent qubits 
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: target_qubit : const size_t :: The target qubit 
+ * :: adj : struct adjacency_obj* :: Pointer to object to write to
+ * Returns a heap allocated array of uint64_t objects
+ */
+void widget_get_adjacencies_api(const widget_t* wid, const size_t target_qubit, struct adjacency_obj* adj)
+{
+    *adj = widget_get_adjacencies(wid, target_qubit); 
+}
+
+/*
+ * widget_destroy_adjacencies 
+ * Frees adjacency resources
+ * :: adj : struct adjacency_obj :: Adjacency object
+ * Frees the array of adjacencies from the object
+ */
+void widget_destroy_adjacencies(struct adjacency_obj* adj)
+{
+    free(adj->adjacencies);
+}
+
+/*
+ * widget_get_io_map_api
+ * Returns an array of io map objects
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: arr : uint32_t** :: Pointer to io map to return 
+ * Writes the pointer to the io map to *arr
+ */
+void widget_get_io_map_api(const widget_t* wid, qubit_map_t** arr)
+{
+    *arr = wid->q_map;
+}
+
+/*
+ * widget_get_local_cliffords_api
+ * Returns an array of local clifford objects
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: arr : uint32_t** :: Pointer to io map to return 
+ * Writes the pointer to the io map to *arr
+ */
+void widget_get_local_cliffords_api(const widget_t* wid, instruction_t** arr)
+{
+    *arr = wid->queue->table;
+}
+
+/*
+ * widget_get_measurements_api
+ * Returns an array of measurements objects
+ * :: wid : const widget_t* :: The widget to get adjacencies for 
+ * :: arr : uint32_t** :: Pointer to io map to return 
+ * Writes the pointer to the io map to *arr
+ */
+void widget_get_measurement_tags_api(const widget_t* wid, non_clifford_tag_t** arr)
+{
+    *arr = wid->queue->non_cliffords;
+}
