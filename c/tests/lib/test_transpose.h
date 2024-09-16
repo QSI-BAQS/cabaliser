cdf5d15c5ae8a972f2d542010d512435d272b994
Alan Robertson <alan@qrop.xyz>
2024-09-16 22:05:55 +1000

    Adding transpose test

---
+++ c/tests/lib/test_transpose.h
@@ -0,0 +1,19 @@
+#ifndef TEST_TRANSPOSE_H
+#define TEST_TRANSPOSE_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <assert.h>
+
+#include <emmintrin.h>
+#include <smmintrin.h>
+#include <immintrin.h>
+
+#include <x86gprintrin.h>
+
+// Due to compiler optimisations, some tweaking with flags is needed here.
+
+#endif
