0cf06532bd0e8196bbc85bf332023a17570981d4
Alan Robertson <alan@qrop.xyz>
2024-09-19 18:37:25 +1000

    Moved pandora dag layering to inefficient stored procedure

---
+++ c/pandora_api/sql/decorate_dag.sql
@@ -0,0 +1,96 @@
+CREATE OR REPLACE PROCEDURE unvisit_circuit()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+UPDATE linked_circuit_qubit 
+    SET visited = FALSE; 
+COMMIT;
+END;$$;
+
+
+CREATE OR REPLACE PROCEDURE decorate_input()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+-- Initial decorated circuit 
+INSERT INTO decorated_circuit (
+    SELECT id, 0 
+    FROM linked_circuit_qubit 
+    WHERE type = 'In'
+        AND visited = FALSE);
+
+UPDATE linked_circuit_qubit 
+    SET visited = TRUE 
+    WHERE linked_circuit_qubit.id = (
+        SELECT decorated_circuit.id
+        FROM decorated_circuit
+        WHERE linked_circuit_qubit.id = decorated_circuit.id
+    );
+
+COMMIT;
+END;$$;
+
+CREATE OR REPLACE PROCEDURE decorate_layer(
+    layer_num int
+    )
+LANGUAGE plpgsql
+AS $$
+BEGIN
+-- Decorate the next layer
+INSERT INTO decorated_circuit (
+    SELECT id, layer_num FROM linked_circuit_qubit 
+    WHERE visited = FALSE 
+        AND (prev_q1  / 10 = ( 
+            SELECT id 
+                FROM decorated_circuit 
+                WHERE linked_circuit_qubit.prev_q1 / 10 = decorated_circuit.id))
+        AND (prev_q2 IS NULL OR
+            prev_q2 / 10 = (
+            SELECT id 
+                FROM decorated_circuit 
+                WHERE linked_circuit_qubit.prev_q2 / 10 = decorated_circuit.id))
+        AND (prev_q3 IS NULL OR
+            prev_q3 / 10 = (
+            SELECT id 
+                FROM decorated_circuit 
+                WHERE linked_circuit_qubit.prev_q3 / 10 = decorated_circuit.id))
+    );
+COMMIT;
+
+-- Set the decorated nodes to visited 
+UPDATE linked_circuit_qubit 
+    SET visited = TRUE 
+    WHERE linked_circuit_qubit.id = (
+        SELECT decorated_circuit.id
+        FROM decorated_circuit
+        WHERE layer = layer_num
+            AND linked_circuit_qubit.id = decorated_circuit.id
+    );
+
+-- Commit the changes
+COMMIT;
+END;$$;
+
+-- Loop to parse the whole circuit
+CREATE OR REPLACE PROCEDURE decorate_circuit()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+   layer integer := 1; 
+BEGIN
+-- Reset all nodes as unvisited
+CALL unvisit_circuit();
+
+-- Decorate the input nodes
+CALL decorate_input();
+
+-- Loop for each layer
+WHILE EXISTS(SELECT NULL FROM linked_circuit_qubit WHERE visited = FALSE) LOOP
+    CALL decorate_layer(layer);
+    layer := layer + 1;
+END LOOP;
+
+-- Commit to be safe
+COMMIT;
+END;$$;
+
