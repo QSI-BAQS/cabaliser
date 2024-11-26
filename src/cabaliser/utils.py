c99e93b4d8135888a3fb7e9664a930af6012aec3
jqxcz <jqxczz@gmail.com>
2024-11-26 11:04:33 +1100

    Add subclass of c_void_p for restype ctype conversion

--- src/cabaliser/utils.py
+++ src/cabaliser/utils.py
@@ -5,6 +5,13 @@ from ctypes import c_void_p
 
 INF = float('inf')
 
+class void_p(c_void_p):
+    def __format__(self, *args, **kwargs):
+        return super().__format__(*args, **kwargs)
+    def __repr__(self, *args, **kwargs):
+        return super().__repr__(*args, **kwargs)
+    # Subclass (see https://stackoverflow.com/questions/17840144/why-does-setting-ctypes-dll-function-restype-c-void-p-return-long)
+
 
 def deref(obj: c_void_p) -> object:
     '''
