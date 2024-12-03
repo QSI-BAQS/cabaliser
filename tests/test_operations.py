8ed4bebfead2ba42c69e1918f1e38d49973520d1
Alan Robertson <alan@qrop.xyz>
2024-12-03 14:44:52 +1100

    Added tests for local cliffords

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,7 +5,9 @@ from cabaliser import gates
 from cabaliser.operation_sequence import OperationSequence 
 from cabaliser.widget import Widget
 from cabaliser import local_simulator 
+from functools import reduce
 
+MAX_CLIFFORD_DEPTH = 2
 EPS = 1e-7
 N_REPETITIONS = 100
 class OperationTest(unittest.TestCase):
@@ -45,11 +47,88 @@ class OperationTest(unittest.TestCase):
             effective_state = input_state
             embedded_state = local_simulator.kr(*([zero_state] * 2 * n_inputs), effective_state) 
             traced_state = local_simulator.trace_out_graph(wid, widget_state)
-                                                                                              
+                     
             assert (np.abs(embedded_state - widget_state) < EPS).all()
             assert (np.abs(traced_state - effective_state) < EPS).all()                              
 
+    def test_all_local_cliffords(self, seed=0):
+        '''
+           Sequence of random local cliffords 
+        '''
+        n_qubits = 1
+        max_qubits = 3
+        
+        #for i in N_REPETITIONS: 
 
+        # Test all two qubit clifford sequences
+        
+        generator = np.random.default_rng()
+        for op_a in gates.SINGLE_QUBIT_GATES:
+            for op_b in gates.SINGLE_QUBIT_GATES:
+                operations = [(op_a, (0,)), (op_b, (0,))]
+
+                wid = Widget(n_qubits, max_qubits)
+
+                ops = OperationSequence(len(operations))
+                for opcode, args in operations: 
+                    ops.append(opcode, *args)
+
+                operation = reduce(
+                    lambda a, b: b @ a,
+                    map(lambda x: local_simulator.LOCAL_CLIFFORD_OP_TABLE[x[0]], operations),
+                    local_simulator.I
+                 )
+
+                wid(ops) 
+                wid.decompose()
+
+                wid_operation = wid.get_local_cliffords().to_list()[1]
+                if not cmp_gates(local_simulator.LOCAL_CLIFFORD_TABLE[wid_operation], operation):
+
+                    print("OPs:", list(map(gates.SINGLE_QUBIT_GATE_ARR.__getitem__, map(lambda x: x[0], operations))))
+                    print("Operation: ", operation)
+                    print(wid_operation)
+                    print(local_simulator.LOCAL_CLIFFORD_TABLE[wid_operation])
+                    print(wid.get_local_cliffords().to_list())
+
+                    assert False
+
+                ## Test application to random states
+                for _ in range(N_REPETITIONS):
+                    input_state = local_simulator.state_prep(
+                        *list(np.random.random(2))
+                    )
+
+                    widget_state = local_simulator.simulate_widget(
+                         wid,
+                         input_state=input_state,
+                         trace_graph=True
+                    )
+                    effective_state = (
+                         operation @ input_state
+                    )
+                    
+                    eff_const = effective_state[0][0]
+                    wid_const = widget_state[0][0] 
+                    if eff_const == 0:
+                        eff_const = effective_state[0][1] 
+                    if wid_const == 0:
+                        wid_const = widget_state[0][1]
+                    if (np.abs(effective_state / eff_const - widget_state / wid_const) > EPS).any():
+                        print("Operation: ", operation)
+                        print(input_state)
+                        print(effective_state)
+                        print(widget_state)
+                        assert False
+
+#    def test_random_cliffords(self):
+#
+#        for i in N_REPETITIONS:
+#        operations = list(map(lambda x: (x, (0,)), generator.choice(
+#            tuple(gates.SINGLE_QUBIT_GATES),
+#            MAX_CLIFFORD_DEPTH,
+#            replace=True))) 
+#
 
     def test_hadamard(self): 
         '''
@@ -393,5 +472,29 @@ class OperationTest(unittest.TestCase):
             )
             assert (np.abs(input_state - widget_state) < EPS).all()
 
+
+def cmp_gates(gate_a, gate_b, eps=1e-5):
+    '''
+        Testing gate equivalence
+    '''
+    tol = int(-1 * np.log10(eps))
+    n_qubits = int(np.log2(gate_a.shape[0]))
+    
+    consts = None
+    for basis in range(1 << n_qubits):
+        basis_state = np.zeros(1 << n_qubits, dtype=np.complex128)  
+        basis_state[basis] = 1 
+
+        state = np.round(gate_a.transpose().conj() @ gate_b @ basis_state, tol) 
+        if sum(map(lambda x: (x > eps), state)) > 1: 
+            return False
+        if consts is None:     
+            consts = state[basis] 
+        if any(map(lambda x: np.abs(x[0] - consts * x[1]) > eps, zip(state, basis_state))):
+            return False
+    return True
+
+
 if __name__ == '__main__':
+    #OperationTest().test_random_local_cliffords()
     unittest.main()
