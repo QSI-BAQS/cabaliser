db01c619b5af6fdb77d29d360d81ffbddc6732df
Alan Robertson <alan@qrop.xyz>
2025-01-07 09:02:54 +1100

    Linting

--- tests/test_cliffords.py
+++ tests/test_cliffords.py
@@ -1,14 +1,16 @@
 import unittest
-import numpy as np
-from math import isnan
 
 from itertools import combinations_with_replacement, permutations, chain
 from functools import reduce
 
+from math import isnan
+
+import numpy as np
+
 from cabaliser import gates
-from cabaliser.operation_sequence import OperationSequence 
+from cabaliser.operation_sequence import OperationSequence
 from cabaliser.widget import Widget
-from cabaliser import local_simulator 
+from cabaliser import local_simulator
 
 MAX_CLIFFORD_DEPTH = 12
 EPS = 1e-7
@@ -52,13 +54,12 @@ class CliffordsTest(unittest.TestCase):
 
     def clifford_sequences(self, depth=1, test_states=True, test_gates=True, apply=False):
         '''
-           Sequence of random local cliffords 
+           Sequence of random local cliffords
         '''
         n_qubits = 1
         max_qubits = 3
-        
+
         # Test all two qubit clifford sequences
-        
         for op_seq in combinations_with_replacement(gates.SINGLE_QUBIT_GATES, depth):
             for op_perm in permutations(op_seq):
                 operations = [(op, (0,)) for op in op_perm]
@@ -66,7 +67,7 @@ class CliffordsTest(unittest.TestCase):
                 wid = Widget(n_qubits, max_qubits)
 
                 ops = OperationSequence(len(operations))
-                for opcode, args in operations: 
+                for opcode, args in operations:
                     ops.append(opcode, *args)
 
                 operation = reduce(
@@ -75,15 +76,15 @@ class CliffordsTest(unittest.TestCase):
                     local_simulator.I
                  )
 
-                wid(ops) 
+                wid(ops)
 
                 if apply:
                     wid.apply_local_cliffords()
-            
+
                 wid.decompose()
 
 
-                if test_gates:  # Only tests the clifford composition sequences 
+                if test_gates:  # Only tests the clifford composition sequences
                     wid_operation = wid.get_local_cliffords().to_list()[1]
                     if not cmp_gates(local_simulator.LOCAL_CLIFFORD_TABLE[wid_operation], operation):
 
@@ -94,12 +95,12 @@ class CliffordsTest(unittest.TestCase):
                         print(wid.get_local_cliffords().to_list())
 
                         assert False
-                
+
                 if test_states:
                     ## Test application to random states
-                    for _ in range(1):#N_REPETITIONS):
+                    for _ in range(N_REPETITIONS):
                         input_state = local_simulator.state_prep(
-                           1, 0 #*list(np.random.random(2))
+                           *list(np.random.random(2))
                         )
 
                         widget_state = local_simulator.simulate_widget(
@@ -110,7 +111,7 @@ class CliffordsTest(unittest.TestCase):
                         effective_state = (
                              operation @ input_state
                         )
-                        
+
                         dephase(widget_state)
                         dephase(effective_state)
 
@@ -126,16 +127,16 @@ class CliffordsTest(unittest.TestCase):
         generator = np.random.default_rng(seed)
         n_qubits = 1
         max_qubits = 3
-        for i in range(N_REPETITIONS):
+        for _ in range(N_REPETITIONS):
             operations = list(map(lambda x: (x, (0,)), generator.choice(
             tuple(gates.SINGLE_QUBIT_GATES),
             MAX_CLIFFORD_DEPTH,
-            replace=True))) 
+            replace=True)))
 
             wid = Widget(n_qubits, max_qubits)
 
             ops = OperationSequence(len(operations))
-            for opcode, args in operations: 
+            for opcode, args in operations:
                 ops.append(opcode, *args)
 
             operation = reduce(
@@ -144,7 +145,7 @@ class CliffordsTest(unittest.TestCase):
                 local_simulator.I
              )
 
-            wid(ops) 
+            wid(ops)
             wid.decompose()
 
             wid_operation = wid.get_local_cliffords().to_list()[1]
@@ -160,11 +161,11 @@ class CliffordsTest(unittest.TestCase):
 
     def clifford_sequences_cnot_first(self, depth=1, test_states=True):
         '''
-           Sequence of random local cliffords on one qubit, followed by a CNOT 
+           Sequence of random local cliffords on one qubit, followed by a CNOT
         '''
         n_qubits = 2
         max_qubits = 4
-        
+
         for op_seq in combinations_with_replacement(gates.SINGLE_QUBIT_GATES, depth):
             for op_perm in permutations(op_seq):
                 operations = [(op, (0,)) for op in op_perm]
@@ -173,9 +174,9 @@ class CliffordsTest(unittest.TestCase):
                 wid = Widget(n_qubits, max_qubits)
 
                 ops = OperationSequence(len(operations))
-                for opcode, args in operations: 
+                for opcode, args in operations:
                     ops.append(opcode, *args)
-    
+
                 operation = reduce(
                     lambda a, b: b @ a,
                     map(lambda x: local_simulator.LOCAL_CLIFFORD_OP_TABLE[x[0]], operations[:-1]),
@@ -183,7 +184,7 @@ class CliffordsTest(unittest.TestCase):
                  )
                 operation = local_simulator.CNOT(2, 0, 1) @ local_simulator.kr(operation, local_simulator.I)
 
-                wid(ops) 
+                wid(ops)
                 wid.decompose()
 
                 ## Test application to random states
@@ -217,22 +218,22 @@ class CliffordsTest(unittest.TestCase):
 
     def clifford_sequences_cz_first(self, depth=1, test_states=True):
         '''
-           Sequence of random local cliffords on one qubit, followed by a CZ 
+           Sequence of random local cliffords on one qubit, followed by a CZ
         '''
         n_qubits = 2
         max_qubits = 4
-        
+
         for op_seq in combinations_with_replacement(gates.SINGLE_QUBIT_GATES, depth):
             for op_perm in permutations(op_seq):
                 operations = [(op, (0,)) for op in op_perm]
                 operations.append((gates.CZ, (0, 1)))
-                
+
                 wid = Widget(n_qubits, max_qubits)
 
                 ops = OperationSequence(len(operations))
-                for opcode, args in operations: 
+                for opcode, args in operations:
                     ops.append(opcode, *args)
-    
+
                 operation = reduce(
                     lambda a, b: b @ a,
                     map(lambda x: local_simulator.LOCAL_CLIFFORD_OP_TABLE[x[0]], operations[:-1]),
@@ -240,7 +241,7 @@ class CliffordsTest(unittest.TestCase):
                  )
                 operation = local_simulator.CZ(2, 0, 1) @ local_simulator.kr(operation, local_simulator.I)
 
-                wid(ops) 
+                wid(ops)
                 wid.decompose()
 
                 ## Test application to random states
@@ -277,25 +278,25 @@ def cmp_gates(gate_a, gate_b, eps=1e-5):
     '''
     tol = int(-1 * np.log10(eps))
     n_qubits = int(np.log2(gate_a.shape[0]))
-    
+
     consts = None
     for basis in range(1 << n_qubits):
-        basis_state = np.zeros(1 << n_qubits, dtype=np.complex128)  
-        basis_state[basis] = 1 
+        basis_state = np.zeros(1 << n_qubits, dtype=np.complex128)
+        basis_state[basis] = 1
 
-        state = np.round(gate_a.transpose().conj() @ gate_b @ basis_state, tol) 
-        if sum(map(lambda x: (x > eps), state)) > 1: 
+        state = np.round(gate_a.transpose().conj() @ gate_b @ basis_state, tol)
+        if sum(map(lambda x: (x > eps), state)) > 1:
             return False
-        if consts is None:     
-            consts = state[basis] 
+        if consts is None:
+            consts = state[basis]
         if any(map(lambda x: np.abs(x[0] - consts * x[1]) > eps, zip(state, basis_state))):
             return False
     return True
 
-def dephase(vector: np.array) -> bool: 
+def dephase(vector: np.array) -> bool:
     '''
     Divide whole vector by first non-zero element
-    Returns False if the vector is empty, and True 
+    Returns False if the vector is empty, and True
     if the operation completed successfully
 
     Acts in place on the vector object
@@ -306,7 +307,7 @@ def dephase(vector: np.array) -> bool:
                         lambda x: np.abs(x) > EPS,
                         chain(iter(vector))
                     )
-              ) 
+              )
         if isnan(val[0].real):
             return False
         vector /= val
@@ -316,7 +317,4 @@ def dephase(vector: np.array) -> bool:
 
 
 if __name__ == '__main__':
-    #CliffordsTest().test_cnot_clifford_depth_2()
-    #CliffordsTest().test_clifford_depth_3_applied()
-
     unittest.main()
