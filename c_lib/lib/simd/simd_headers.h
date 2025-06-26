776762a1e88bafa84f3574c0700a6be2dd5cc508
Ben Y. <bjy777101@gmail.com>
2025-06-26 14:34:45 +1000

    Added arm features to header files
    Added conditional compilation for __arm__ (this may need a clang version
    too) for some defines and for includes in simd_headers
    Added define "TABLEAU_SIMD_VEC" for __arm__ (the datatype for the
    vectors, currently `uint8x16_t`)
    Added define "NEON_SUF" for __arm__ (all Neon intrinsics have type
    suffixes, currently `u8` to match above type)

--- c_lib/lib/simd/simd_headers.h
+++ c_lib/lib/simd/simd_headers.h
@@ -1,10 +1,18 @@
 #ifndef SIMD_HEADERS_H
 #define SIMD_HEADERS_H
 
+// TODO - these might be gcc only
+#if defined(__x86_64__)
 #include <emmintrin.h>
 #include <smmintrin.h>
 #include <immintrin.h>
 #include <x86gprintrin.h>
+#elif defined(__arm__) || defined(__aarch64__)
+#include <arm_neon.h>
+// The suffix for Neon function calls (must match TABLEAU_SIMD_VEC type)
+// u8 is for vectors of uint8s (ie. a uint8x16_t)
+#define NEON_SUF u8
+#endif
 
 #define ROWSUM_STRIDE (256 / 8) 
 
