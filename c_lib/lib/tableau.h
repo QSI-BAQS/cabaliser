72b2e284a0c2676639ebd9858dc514c7ef8e60b7
Ben Y. <bjy777101@gmail.com>
2025-07-08 11:02:44 +1000

    Corrected typo in header file "x86-64" -> "x86_64"

--- c_lib/lib/tableau.h
+++ c_lib/lib/tableau.h
@@ -52,7 +52,7 @@ typedef struct tableau_t tableau_t;
 #define CHUNK_IDX(slice, offset_byte) (*(CHUNK_OBJ*)((void*)slice + offset_byte))
 
 // TODO - This may be gcc only
-#if defined(__x86-64__)
+#if defined(__x86_64__)
 #define TABLEAU_SIMD_VEC __m256i
 #elif defined(__arm__) || defined(__aarch64__)
 #define TABLEAU_SIMD_VEC uint8x16_t
