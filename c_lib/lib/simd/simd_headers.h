c655f0cfc63db2dc7876bfbe54c9584a05505532
Ben Y. <bjy777101@gmail.com>
2025-07-09 11:04:36 +1000

    Updated simd headers to use sve(2) alongside neon, with a bridge
    This allows the use of the sve ext and dup instructions (not available
    in neon)

--- c_lib/lib/simd/simd_headers.h
+++ c_lib/lib/simd/simd_headers.h
@@ -1,7 +1,6 @@
 #ifndef SIMD_HEADERS_H
 #define SIMD_HEADERS_H
 
-// TODO - these might be gcc only
 #if defined(__x86_64__)
 #include <emmintrin.h>
 #include <smmintrin.h>
@@ -10,9 +9,8 @@
 #define ROWSUM_STRIDE (256 / 8) 
 #elif defined(__arm__) || defined(__aarch64__)
 #include <arm_neon.h>
-// The suffix for Neon function calls (must match TABLEAU_SIMD_VEC type)
-// u8 is for vectors of uint8s (ie. a uint8x16_t)
-#define NEON_SUFFIX u8
+#include <arm_sve.h>
+#include <arm_neon_sve_bridge.h>
 // Halved ROWSUM_STRIDE for Neon
 #define ROWSUM_STRIDE (128 / 8) 
 #endif
